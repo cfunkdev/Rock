baab56cb109078fc9f03f12de01629c88c08856b
Kwame_Agyei <kwame.ajyei@techvision.global>
2023-12-14 11:38:49 +0000

    - Added obsidian RemoteAuths Field Type.

--- Rock.JavaScript.Obsidian/Framework/FieldTypes/index.ts
+++ Rock.JavaScript.Obsidian/Framework/FieldTypes/index.ts
@@ -332,6 +332,9 @@ registerFieldType(FieldTypeGuids.ReminderType, new ReminderTypeFieldType());
 import { ReminderTypesFieldType } from "./reminderTypesField.partial";
 registerFieldType(FieldTypeGuids.ReminderTypes, new ReminderTypesFieldType());
 
+import { RemoteAuthsFieldType } from "./remoteAuthsField.partial";
+registerFieldType(FieldTypeGuids.RemoteAuths, new RemoteAuthsFieldType());
+
 import { ReportFieldType } from "./reportField.partial";
 registerFieldType(FieldTypeGuids.Report, new ReportFieldType());
 
